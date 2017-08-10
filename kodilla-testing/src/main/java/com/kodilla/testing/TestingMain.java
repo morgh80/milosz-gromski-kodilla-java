f169b006bae5621975f43cf88ad54ff6f3e04564
aeronaut <aeronaut@MacBook-Pro-aeronaut.local>
2017-08-10 19:33:24 +0200

    Unit tests (add, subtract) on TestingMain class added

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,28 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
-    public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+    public static void main(String[] args) {
+
+        Calculator calculator = new Calculator();
+
+        //Calculator add function test
+        Double addResult = calculator.add(1.0, 2.0);
+        if (addResult.equals(3.0)) {
+            System.out.println("Add test OK");
+        } else {
+            System.out.println("Add test failed");
+        }
+
+        //Calculator subtract function test
+        Double subtractResult = calculator.subtract(2.0, 1.0);
+        if (subtractResult.equals(1.0)) {
+            System.out.println("Subtract test OK");
+        } else {
+            System.out.println("Subtract test failed");
+        }
+
     }
 }
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public Double add(Double a, Double b) {
+        return a + b;
+    }
+
+    public Double subtract(Double a, Double b) {
+        return a - b;
+    }
+}
